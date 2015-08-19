ed6f7fc1e9c07f5816a59098c73d84947d1717ff
Fabio Kuhn <mordaroso@gmail.com>
2015-08-19 14:21:10 -0400

    Add default test rake task

--- test/marinetraffic/test_vessel.rb
+++ test/marinetraffic/test_vessel.rb
@@ -1,4 +1,4 @@
-require 'minitest_helper'
+require File.expand_path('../../minitest_helper.rb', __FILE__)
 
 module Marinetraffic
   class VesselTest < Minitest::Test
--- test/test_marinetraffic.rb
+++ test/test_marinetraffic.rb
@@ -1,4 +1,4 @@
-require 'minitest_helper'
+require File.expand_path('../minitest_helper.rb', __FILE__)
 
 class TestMarinetraffic < Minitest::Test
   def test_that_it_has_a_version_number
