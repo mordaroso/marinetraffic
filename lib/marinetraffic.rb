4b5eb79359e34e817933dd8f96e8696f1c3cf8f4
Fabio Kuhn <mordaroso@gmail.com>
2015-08-26 14:09:35 -0400

    Move api base to class variable

--- lib/marinetraffic.rb
+++ lib/marinetraffic.rb
@@ -3,7 +3,9 @@ require "marinetraffic/vessel"
 require "marinetraffic/api"
 
 module Marinetraffic
+  @api_base = 'http://services.marinetraffic.com/api'
+
   class << self
-    attr_accessor :api_key
+    attr_accessor :api_key, :api_base
   end
 end
--- lib/marinetraffic/api.rb
+++ lib/marinetraffic/api.rb
@@ -1,14 +1,12 @@
 require 'faraday'
 require 'nokogiri'
 
-API_URL = "http://services.marinetraffic.com/api"
-
 module Marinetraffic
   class API
     def self.call(name, params = {})
       params[:protocol] = :xml
       param_string = params.map{|attr| "#{attr.first}:#{attr.last}"}.join('/')
-      url = "#{API_URL}/#{name}/#{Marinetraffic.api_key}/#{param_string}"
+      url = "#{Marinetraffic.api_base}/#{name}/#{Marinetraffic.api_key}/#{param_string}"
       response = Faraday.get(url)
       doc = Nokogiri::XML(response.body)
       raise_exception(doc) if doc.xpath('//ERROR').any?
